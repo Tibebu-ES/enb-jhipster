79615bcd40a7a9e8e40aaf74572c5c2c03727af7
Tibebu-ES <tibebuenyew@gmail.com>
2021-03-10 19:22:55 +0300

    Initial version of enb generated by JHipster-6.10.5

---
+++ src/main/java/org/jhipster/enb/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.jhipster.enb.service.dto;
---
+++ src/main/java/org/jhipster/enb/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.jhipster.enb.web.rest.vm;
